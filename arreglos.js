2045a7d4586c6c2c006926610f50bab450573283
OskarPaul93 <paul_1993oscar@hotmail.com>
2026-05-03 23:52:15 -0500

    Boton eliminar y mover izauierda

--- arreglos.js
+++ arreglos.js
@@ -30,10 +30,55 @@ function pintarArregloIzquierdo (){
         //contenidoTabla+= "<td>" + <button class="btn-eliminar">Eliminar</button> + "</td>";
         //contenidoTabla+= "<td>" + <button class="btn-mover">➜</button> + "</td>";
 
-        contenidoTabla+= "<td><button class='btn-eliminar'>Eliminar</button> </td><td> <button class='btn-mover'>➜</button></td>"
-
+        contenidoTabla+= "<td><button class='btn-eliminar' onclick='eliminarIzquierdo(" + i + ")' >Eliminar</button> </td>" +
+        "<td><button class='btn-mover' onclick='moverHaciaDerecha (" + i + ")' >➜</button></td>"
+        
         contenidoTabla+= "</tr>";
     }
 
     tbody.innerHTML= contenidoTabla;
+}
+
+
+function pintarArregloDerecha(){
+    let tbody = document.getElementById("tablaDerecha");
+    let contenidoTabla = "";
+
+    for (let i = 0; i < edadesDerecho.length; i++){
+        let edadRecuperada = edadesDerecho[i];
+
+        contenidoTabla += "<tr>";
+        contenidoTabla += "<td><button class= 'btn-mover'onclick='moverHaciaIzquierda(" + i + ")'>⬅</button></td>";
+        contenidoTabla += "<td>" + edadRecuperada + "</td>";
+        contenidoTabla += "<td><button class= 'btn-eliminar' onclick='eliminarDerecho(" + i + ")'>Eliminar</button></td>";
+        contenidoTabla += "</tr>";
+    }
+
+    tbody.innerHTML = contenidoTabla;
+}
+
+function eliminarDerecho(indice){
+    edadesDerecho.splice(indice, 1);
+    pintarArregloDerecha();
+}
+
+
+function moverHaciaDerecha(indice){
+    let edad = edadesIzquierdo[indice];
+
+    edadesDerecho.push(edad);
+    edadesIzquierdo.splice(indice, 1);
+
+    pintarArregloIzquierdo();
+    pintarArregloDerecha();
+}
+
+function moverHaciaIzquierda(indice){
+    let edad = edadesDerecho[indice];
+
+    edadesIzquierdo.push(edad);
+    edadesDerecho.splice(indice, 1);
+
+    pintarArregloDerecha();
+    pintarArregloIzquierdo();
 }
